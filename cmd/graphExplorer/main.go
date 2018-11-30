5176598b34c0745b0dfc2964c48b985356f7cc77
Saurabh Deoras <sdeoras@gmail.com>
2018-11-30 14:29:14 -0800

    added graph explorer (#2)

---
+++ cmd/graphExplorer/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/sdeoras/go-scicomp/common"
+)
+
+type OpInfo struct {
+	Name       string
+	Device     string
+	Type       string
+	NumInputs  int
+	NumOutputs int
+}
+
+func main() {
+	fileName := flag.String("file", "", "graph file name")
+	flag.Parse()
+
+	if *fileName == "" {
+		return
+	}
+
+	op := new(common.Op)
+	if err := common.InitUsingGraphFile(op, *fileName, nil); err != nil {
+		log.Fatal(err)
+	}
+	defer op.Close()
+
+	for _, operation := range op.GetGraph().Operations() {
+		info := OpInfo{
+			Name:       operation.Name(),
+			Device:     operation.Device(),
+			Type:       operation.Type(),
+			NumInputs:  operation.NumInputs(),
+			NumOutputs: operation.NumOutputs(),
+		}
+
+		if jb, err := json.Marshal(info); err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println(string(jb))
+		}
+	}
+}
--- op/layers/tf.go
+++ op/layers/tf.go
@@ -19,7 +19,7 @@ type Op struct {
 // with TF.
 func NewOperator(options *tf.SessionOptions) (*Op, error) {
 	op := new(Op)
-	if err := common.InitOperator(op, modelPB, options); err != nil {
+	if err := common.InitUsingB64Graph(op, modelPB, options); err != nil {
 		return nil, err
 	}
 	return op, nil
